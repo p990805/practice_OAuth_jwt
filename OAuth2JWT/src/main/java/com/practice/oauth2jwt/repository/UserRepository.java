22f5a6f7cb78faf1d56d68d200bf2fb4807841be
박주찬 <p990805@naver.com>
2025-01-05 23:08:12 +0900

    [주찬] Feat : jwt 로그인 중 oauth2 jwt발급x 로그인까지 구현  0105

---
+++ OAuth2JWT/src/main/java/com/practice/oauth2jwt/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.practice.oauth2jwt.repository;
+
+import com.practice.oauth2jwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findByUsername(String username);
+}

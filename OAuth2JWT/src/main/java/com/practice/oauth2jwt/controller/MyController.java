83dca2d0bff90349295fcbb6c6ad45b38a337301
박주찬 <p990805@naver.com>
2025-01-05 13:52:47 +0900

    [주찬] Feat : 프로젝트 생성 01.05

---
+++ OAuth2JWT/src/main/java/com/practice/oauth2jwt/controller/MyController.java
@@ -0,0 +1,13 @@
+package com.practice.oauth2jwt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/my")
+    public String myAPI(){
+        return "my Route";
+    }
+}

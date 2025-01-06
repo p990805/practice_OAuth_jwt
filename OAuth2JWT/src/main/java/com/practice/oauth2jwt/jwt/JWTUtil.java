4f2e7f21c197326a39fd06661509f21e6b7686b2
박주찬 <p990805@naver.com>
2025-01-06 23:19:29 +0900

    [주찬] Feat : CustomSuccessHanler 구현 후 SecurityConfig에 주입01.06

--- OAuth2JWT/src/main/java/com/practice/oauth2jwt/jwt/JWTUtil.java
+++ OAuth2JWT/src/main/java/com/practice/oauth2jwt/jwt/JWTUtil.java
@@ -1,8 +1,6 @@
 package com.practice.oauth2jwt.jwt;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -12,11 +10,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
-public class JWTUtill {
+public class JWTUtil {
 
     private SecretKey secretKey;
 
-    public JWTUtill(@Value("${spring.jwt.secret")String secret){
+    public JWTUtil(@Value("${spring.jwt.secret")String secret){
         secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
